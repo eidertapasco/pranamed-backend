3f6647caa937146fcc4b22dce5ef360306365cc0
Eider Tapasco <eidertapasco.dev@gmail.com>
2026-04-15 15:22:52 -0500

    Pranamed: The folder structure for v1.0.0 was created

---
+++ src/main/java/com/pranamedpos/pranamed/PranamedApplication.java
@@ -0,0 +1,13 @@
+package com.pranamedpos.pranamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PranamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PranamedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pranamedpos/pranamed/PranamedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranamedpos.pranamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PranamedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
